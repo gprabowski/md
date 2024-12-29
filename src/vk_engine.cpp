5b35492337344a4f706868e7ca470e6d32d65002
Pawel Grabowski <gprabowski@outlook.com>
2024-12-29 20:24:15 +0100

    Port initial content to SDL3/Wayland

---
+++ src/vk_engine.cpp
@@ -0,0 +1,94 @@
+//> includes
+#include "vk_engine.h"
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+
+#include <vk_initializers.h>
+#include <vk_types.h>
+
+#include <fmt/core.h>
+
+#include <chrono>
+#include <thread>
+
+VulkanEngine* loadedEngine = nullptr;
+
+VulkanEngine& VulkanEngine::Get() { return *loadedEngine; }
+void VulkanEngine::init()
+{
+    // only one engine initialization is allowed with the application.
+    assert(loadedEngine == nullptr);
+    loadedEngine = this;
+
+    // We initialize SDL and create a window with it.
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
+
+    _window = SDL_CreateWindow(
+        "Vulkan Engine",
+        _windowExtent.width,
+        _windowExtent.height,
+        window_flags);
+
+    assert(_window);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(_window, NULL);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    // everything went fine
+    _isInitialized = true;
+}
+
+void VulkanEngine::cleanup()
+{
+    if (_isInitialized) {
+
+        SDL_DestroyWindow(_window);
+    }
+
+    // clear engine pointer
+    loadedEngine = nullptr;
+}
+
+void VulkanEngine::draw()
+{
+    // nothing yet
+}
+
+void VulkanEngine::run()
+{
+    SDL_Event e;
+    bool bQuit = false;
+
+    // main loop
+    while (!bQuit) {
+        // Handle events on queue
+        while (SDL_PollEvent(&e) != 0) {
+            // close the window when user alt-f4s or clicks the X button
+            if (e.type == SDL_EVENT_QUIT) {
+                bQuit = true;
+	    }
+
+	    if (e.type == SDL_EVENT_WINDOW_MINIMIZED) {
+	        stop_rendering = true;
+	    }
+
+	    if (e.type == SDL_EVENT_WINDOW_MAXIMIZED) {
+	        stop_rendering = false;
+	    }
+        }
+
+        // do not draw if we are minimized
+        if (stop_rendering) {
+            // throttle the speed to avoid the endless spinning
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            continue;
+        }
+
+        draw();
+    }
+}
